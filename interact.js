fc538c4a9c6a80b259787f737103b3e833095e55
NazeerGreene <58005647+NazeerGreene@users.noreply.github.com>
2019-12-10 23:55:59 -0800

    Update interact.js
    Attempting to replace the text within the #newNote textarea for testing purposes, but js won't get the textarea value and it won't replace it.

--- interact.js
+++ interact.js
@@ -1,1 +1,9 @@
-$("#newNote").autoResize();
+var totalNotes = 0;
+
+function writeNote(){
+	var note = document.getElementById("newNote");
+	var val = note.textContent + "!!!";
+	note.textContent = val;
+	
+	console.log(val);
+};
